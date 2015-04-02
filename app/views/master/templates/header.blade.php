e1ed0b933060ba463a024236b8e1cc67d0562faf
Matt Crandell <mrcrandell@gmail.com>
2015-04-02 07:03:46 -0400

    Fixed zooming on the site.

--- app/views/master/templates/header.blade.php
+++ app/views/master/templates/header.blade.php
@@ -1,7 +1,7 @@
 <title>{{isset($title) ? $title : ''}}</title>
 <meta name="description" content="{{isset($description) ? $description : ''}}">
 
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
 <link href='http://fonts.googleapis.com/css?family=Crimson+Text:400,400italic,600,600italic,700,700italic' rel='stylesheet' type='text/css'>
 <link href='http://fonts.googleapis.com/css?family=Droid+Sans:400,700' rel='stylesheet' type='text/css'>
