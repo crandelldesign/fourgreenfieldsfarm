01ddfc25f8addaa4cf93492c77aa0ae9683963f8
Matt Crandell <mrcrandell@gmail.com>
2019-09-03 22:51:33 -0400

    Updated dates.

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -30,7 +30,7 @@
 <div class="row hours-prices">
     <div class="col-sm-6 col-md-5">
         <h3>Corn Maze</h3>
-        <p>September 27 to October 28</p>
+        <p>September 28 to October 27</p>
         <h4>Hours</h4>
         <table class="table">
             <tr>
@@ -57,7 +57,7 @@
     </div>
     <div class="col-sm-6 col-md-5 col-md-offset-2">
         <h3>Haunted Maze</h3>
-        <p>October 5 to October 26</p>
+        <p>October 4 to October 26</p>
         <h4>Hours</h4>
         <table class="table">
             <tr>
