d9dd6c22c222d3a5af6329e24c3ee76fdc0adb43
Matt Crandell <mrcrandell@gmail.com>
2020-09-07 22:53:45 -0400

    Updated dates and added covid information.

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -6,6 +6,11 @@
 
 @section('content')
 
+<div class="alert alert-warning">
+    <p><strong>COVID-19 Information</strong></p>
+    <p class="margin-bottom-0">We are an outside venue so masks are encouraged but not mandatory at the maze where there is plenty of room to do social distancing.</p>
+</div>
+
 <h1>Welcome to the Four Green Fields Farm</h1>
 
 <div class="img-right-sm">
@@ -30,7 +35,7 @@
 <div class="row hours-prices">
     <div class="col-sm-6 col-md-5">
         <h3>Corn Maze</h3>
-        <p>September 28 to October 27</p>
+        <p>September 26 to November 1</p>
         <h4>Hours</h4>
         <table class="table">
             <tr>
@@ -57,7 +62,7 @@
     </div>
     <div class="col-sm-6 col-md-5 col-md-offset-2">
         <h3>Haunted Maze</h3>
-        <p>October 4 to October 26</p>
+        <p>October 9 to October 31</p>
         <h4>Hours</h4>
         <table class="table">
             <tr>
