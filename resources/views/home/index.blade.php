3d449c7bb006dd329db7f87ac1ecd99c31041858
Matt Crandell <mrcrandell@gmail.com>
2021-08-29 20:47:56 -0400

    Updated copy and dates for the fall.

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -35,7 +35,7 @@
 <div class="row hours-prices">
     <div class="col-sm-6 col-md-5">
         <h3>Corn Maze</h3>
-        <p>September 26 to November 1</p>
+        <p>Sunday, September 26 to October 31</p>
         <h4>Hours</h4>
         <table class="table">
             <tr>
@@ -59,10 +59,11 @@
                 <td>$2</td>
             </tr>
         </table>
+        <p><small>Note: Opening day is on a Sunday, September 26 instead of the usual Saturday opening due to a family event</small></p>
     </div>
     <div class="col-sm-6 col-md-5 col-md-offset-2">
         <h3>Haunted Maze</h3>
-        <p>October 9 to October 31</p>
+        <p>October 8 to October 30</p>
         <h4>Hours</h4>
         <table class="table">
             <tr>
