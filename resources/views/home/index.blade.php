933c1728167726649f6b2b16258c8bd1be77cbfa
Matt Crandell <mrcrandell@gmail.com>
2022-09-15 15:19:21 -0400

    Fixed a date.

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -37,7 +37,7 @@
 <div class="row hours-prices">
     <div class="col-sm-6 col-md-5">
         <h3>Corn Maze</h3>
-        <p>Sunday, September 24 to October 30</p>
+        <p>Saturday, September 24 to October 30</p>
         <h4>Hours</h4>
         <table class="table">
             <tr>
