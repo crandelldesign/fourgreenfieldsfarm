9e359f8aaaef1bde1f727f2f08ead598d34d76b1
Matt Crandell <mrcrandell@gmail.com>
2023-06-25 17:55:07 -0400

    updated analytics tracking for frontend pages

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-LCD647NGYQ"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-LCD647NGYQ');
+        </script>
         <meta charset="utf-8">
         @include('layouts.head')
-        {!! Analytics::render() !!}
     </head>
     <body>
         @include('layouts.nav')
