f542429ec0de78d58b9243481ca625ec1353f27b
Matt Crandell <mrcrandell@gmail.com>
2024-01-13 20:57:25 -0500

    Updated wedding page

--- resources/views/home/event-barn.blade.php
+++ resources/views/home/event-barn.blade.php
@@ -32,14 +32,99 @@
   </div>
 
   <p>Our barn also makes a great place to conduct corporate and government training, hold organizational meetings, have a family reunion, host a graduation party, or do a fundraising event. Come out in the fall when our corn maze, pumpkin patch, and horse drawn wagon rides are in full swing for an unforgettable employee picnic or appreciation day.  </p>
+  <h2>Food and Catering</h2>
   <p>We have a list of local caterers that you can select from and design your event menu however you like.  We do not receive any money from these caterers but have selected them because of their reputations for reliability and quality.  We don't want to see your event ruined by a caterer who doesn't show up or fails to provide a quality meal and service.  You do have the option to provide your own caterer we just ask to have the name in advance so we can be sure they are reputable.</p>
-  <p>Music and entertainment is up to you to arrange.</p>
+  <h2>Music and Entertainment</h2>
+  <p>The responsibility of arranging the music and entertainment falls on your shoulders. It's up to you to decide what kind of atmosphere you want to create for your guests. Whether you choose live music, a DJ, or a playlist, make sure it's something that will keep your guests entertained. Remember, the right choice of music and entertainment can make or break your event.</p>
+  <h2>Alcohol</h2>
   <p>Alcohol is permitted on the premises however it can not be sold without a special one day State liquor license.  This is only required if you are going to have a cash bar.  The purchase of beer, wine and spirits is the responsibility of the person booking the barn.  We require the use of a TIPS certified bartender.  TIPS is the State of Michigan approved training program required of all people who serve alcohol.  We have a list of bartenders who meet the criteria you can choose from if you don't have a person who is TIPS trained.  Bookings that will be providing alcohol must obtain a one day liquor liability insurance policy for the event.  These policies are available from a variety of insurance agencies.</p>
+
+  <hr>
+
   <p>We have kept our pricing reasonable so that you can have a wonderful experience without breaking the bank.  We also only book one wedding a weekend so the barn will be available from Friday evening till Sunday at Noon to those who book it.  We also have a small pavilion with picnic tables and grill area that you may utilize if you wish free of charge.  It makes a nice spot for a low key rehearsal dinner or just relaxing after decorating for your wedding.</p>
   <p>Give us a call and arrange a tour of our barn and see for yourself what we have to offer.  We love visitors and showing off our property even if  you don't book with us.  The best way to get in touch is by calling or texting us at 231-580-1463.  You can also email us at <a href="mailto:fourgreenfieldsman@yahoo.com">fourgreenfieldsman@yahoo.com</a> </p>
+
+  <!-- <div class="prices">
+
+    <h2 class="text-center">Four Green Fields Farm<br>Wedding Venue Price List</h2>
+
+    <table class="table prices-table">
+      <tr>
+        <th>Reception Barn</th>
+        <td class="text-right">$2800.00</td>
+      </tr>
+      <tr>
+        <th>Reception Barn and Wedding Site</th>
+        <td class="text-right">$3000.00</td>
+      </tr>
+      <tr>
+        <th>Wedding Carriage</th>
+        <td class="text-right">$150.00</td>
+      </tr>
+      <tr>
+        <th>Damage Deposit (Refundable)</th>
+        <td class="text-right">$500.00</td>
+      </tr>
+    </table>
+
+    <p>Price includes the use of the venue from 4:00 pm to 9:00 pm on Friday.<br>
+    Event time on Saturday.<br>
+    Clean up on Sunday to be completed by 12:00 pm.</p>
+  </div>
+
+  <hr> -->
+
+  <div class="text-center">
+    <button class="btn btn-lg btn-prices" data-toggle="modal" data-target="#prices-modal"><h2>Click here to see current prices</h2></button>
+  </div>
+
+  <hr>
 
   <p><strong>WARNING:</strong><br/>
   This is a country setting so we aren't equipped for Divas or Bridezillas.  They need to go to places that charge at least 3 times as much as us.  We are here for nice people who love the country life and hospitality and don't have an unlimited budget. </p>
 </div>
 
+<div class="modal fade" tabindex="-1" role="dialog" id="prices-modal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h2 class="modal-title text-center">Four Green Fields Farm<br>Wedding Venue Price List</h2>
+      </div>
+      <div class="modal-body">
+        <table class="table prices-table">
+          <tr>
+            <th>Reception Barn</th>
+            <td class="text-right">$2800.00</td>
+          </tr>
+          <tr>
+            <th>Reception Barn and Wedding Site</th>
+            <td class="text-right">$3000.00</td>
+          </tr>
+          <tr>
+            <th>Wedding Carriage</th>
+            <td class="text-right">$150.00</td>
+          </tr>
+          <tr>
+            <th>Damage Deposit (Refundable)</th>
+            <td class="text-right">$500.00</td>
+          </tr>
+        </table>
+
+        <p>Price includes the use of the venue from 4:00 pm to 9:00 pm on Friday.<br>
+        Event time on Saturday.<br>
+        Clean up on Sunday to be completed by 12:00 pm.</p>
+
+      </div>
+    </div>
+  </div>
+</div>
+@stop
+
+@section('scripts')
+<script>
+	$(document).ready(function(){
+    // $('#prices-modal').modal('show');
+  });
+</script>
 @stop
